859a5405e1b812e4cc40988c153475f59b40976d
bruno.mendes <zebruno.dev@gmail.com>
2022-05-29 19:26:34 -0300

    commit inicial

---
+++ src/main/java/com/obrunomendes/rh/GestaoDePessoasApplication.java
@@ -0,0 +1,13 @@
+package com.obrunomendes.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDePessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDePessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obrunomendes/rh/GestaoDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obrunomendes.rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
